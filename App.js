270795e11b3b51c391f1a4d3c3d83798e8bb9722
TribuanaTunggaDewa <mujin.dev@gmail.com>
2019-10-06 01:17:26 +0700

    add 3.Delete_todo

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react'
-import {Text, View, StyleSheet} from 'react-native'
+import { Text, View, StyleSheet,TextInput,Button, FlatList} from 'react-native'
 
 export default class App extends Component{
-  render(){
-    const cars = ['work','swim','study','sleep','run']
-
   
+  constructor(props){
+    super(props)
+    this.state = {
+      text:'',
+      carss: ['work','swim','study','sleep','run'] 
+    }
+  }
+
+  render(){
+    
 //map
   return(
+  
       <View style={style.container}>
-       {cars.map((car)=>{return(<Text style={style.item}>{car}</Text>)})}
+       <View >
+        <TextInput style={style.textfield} placeholder="Ketikkan data yang akan di tambah" onChangeText={(text)=>this.setState({text})} value={this.state.text} />
+       <Button title="Add" onPress={()=>this.setState({ carrs : this.state.carss.push(this.state.text), text:'' })} />    
+       </View>
+        <Text>{this.state.text}</Text>
+        {this.state.carss.map((car)=>{
+          return(
+            <>
+              <Text style={style.item}>{car}</Text>
+              <Button title="delete" onPress={()=>this.setState({ carrs : this.state.carss.splice(this.state.carss.indexOf({car}),1) })} /> 
+            </>
+            )
+        })}
       </View>
     )
 
@@ -18,8 +38,8 @@ export default class App extends Component{
 
 const style = StyleSheet.create({
   container: {
-    padding : 15,
-    marginTop: 4
+    padding : 5,
+    marginTop: 2,
   },
   item: {
     color: 'grey',
@@ -27,5 +47,14 @@ const style = StyleSheet.create({
     borderBottomWidth: 2,
     paddingBottom: 3,
     paddingTop: 3
+  },
+  textfield: {
+    height: 35,
+    borderColor: 'black',
+    borderWidth: 2
+  },
+  pullSide : {
+    flex:1,
+    flexDirection:'column'
   }
 })
