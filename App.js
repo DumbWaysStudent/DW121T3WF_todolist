e75fdabd93f8a2eae0898756889a12e7c391b032
TribuanaTunggaDewa <mujin.dev@gmail.com>
2019-10-06 07:26:36 +0700

    3.add_Delete_Todo

--- App.js
+++ App.js
@@ -7,9 +7,11 @@ export default class App extends Component{
     super(props)
     this.state = {
       text:'',
-      carss: ['work','swim','study','sleep','run'] 
+      carss: [{id:1, item:'work'},{id:2, item:'swim'},{id:3, item:'study'},{id:4, item:'sleep'},{id:5, item:'run'}] 
     }
   }
+
+
 
   render(){
     
@@ -19,17 +21,19 @@ export default class App extends Component{
       <View style={style.container}>
        <View >
         <TextInput style={style.textfield} placeholder="Ketikkan data yang akan di tambah" onChangeText={(text)=>this.setState({text})} value={this.state.text} />
-       <Button title="Add" onPress={()=>this.setState({ carrs : this.state.carss.push(this.state.text), text:'' })} />    
+       <Button title="Add" onPress={()=>{ this.setState({ carrs: this.state.carss.push({id : this.state.carss.length + 1 , item: this.state.text}), text: this.state.text='' }) }} />    
        </View>
         <Text>{this.state.text}</Text>
-        {this.state.carss.map((car)=>{
-          return(
+        {this.state.carss.map((car)=>
+          {
+            return (
             <>
-              <Text style={style.item}>{car}</Text>
-              <Button title="delete" onPress={()=>this.setState({ carrs : this.state.carss.splice(this.state.carss.indexOf({car}),1) })} /> 
+            <Text style={style.item} key={car.id}>{car.id}{car.item}</Text>
+            <Button title="Delete" onPress={()=>{ this.setState({ carrs: this.state.carss.splice(this.state.carss.indexOf(car), 1) }) }}/>
             </>
             )
-        })}
+          }
+          )}
       </View>
     )
 
